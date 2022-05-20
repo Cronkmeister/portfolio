7586b26383d5844421eb524b85348e5f14458bda
Jonny Cronk <cronkmeister@Jonnys-MacBook-Pro.local>
2022-05-20 16:41:23 -0700

    project reusable component created

---
+++ src/components/Project/Project.js
@@ -0,0 +1,33 @@
+import "./Project.scss";
+import { FiGithub, FiExternalLink } from "react-icons/fi";
+
+function Project(props) {
+  return (
+    <>
+      <div className="proj__container--left">
+        <img
+          className="proj__img"
+          src={props.screenshot}
+          alt="project screenshot"
+        ></img>
+      </div>
+      <div className="proj__container--right right">
+        <h3 className="proj__heading">{props.title}</h3>
+        <p className="proj__text">{props.description}</p>
+        <ul className="proj__tech-list">
+          <span className="proj__tech-list--item">{props.tech1}</span>
+          <span className="proj__tech-list--item middle">{props.tech2}</span>
+          <span className="proj__tech-list--item">{props.tech3}</span>
+        </ul>
+        <a href={props.github} target="_blank">
+          <FiGithub className="proj__icon" />
+        </a>
+        <a href={props.website} target="_blank">
+          <FiExternalLink className="proj__icon" />
+        </a>
+      </div>
+    </>
+  );
+}
+
+export default Project;
--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,5 +1,6 @@
 import "./Projects.scss";
 import journal_screenshot from "../../assests/images/screenshot-journal.png";
+import Project from "../../components/Project/Project";
 
 function Projects() {
   return (
@@ -10,26 +11,19 @@ function Projects() {
 
           <div className="proj">
             <div className="proj__container">
-              <div className="proj__container--left">
-                <img
-                  className="proj__img"
-                  src={journal_screenshot}
-                  alt="project screenshot"
-                ></img>
-              </div>
-              <div className="proj__container--right">
-                <h3 className="proj__heading">Journal</h3>
-                <p className="proj__text">
-                  An app to showcase my film photographs in a well desgined
-                  gallery. A place to bring the photos out of the folder where
-                  they were gathering digital dust. View the them in different,
-                  appealing ways. See the locations the photos were taken and
-                  add information alongside them
-                </p>
-                <ul className="proj__tech-list">
-                  <li className="project__tech-list--item">React</li>
-                </ul>
-              </div>
+              <Project
+                screenshot={journal_screenshot}
+                title="Journal"
+                description="An app to showcase my film photographs in a well desgined gallery. A
+              place to bring the photos out of the folder where they were gathering
+              digital dust. View the them in different, appealing ways. See the
+              locations the photos were taken and add information alongside them"
+                tech1="React"
+                tech2="MySQL"
+                tech3="SASS"
+                github="https://github.com/Cronkmeister/journal"
+                website=""
+              />
             </div>
           </div>
         </div>
