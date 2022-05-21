1cfbea0449fdf0212701c568594821c13f1e949c
Jonny Cronk <cronkmeister@Jonnys-MacBook-Pro.local>
2022-05-20 19:35:44 -0700

    projects section expanded

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,6 +1,14 @@
 import "./Projects.scss";
-import journal_screenshot from "../../assests/images/screenshot-journal.png";
 import Project from "../../components/Project/Project";
+import ProjectSideSwap from "../../components/ProjectSideSwap/ProjectSideSwap";
+//project screenshots
+import journal_screenshot from "../../assests/images/screenshot-journal.png";
+import dad_jokes from "../../assests/images/dad-joke-generator.png";
+import brainflix from "../../assests/images/brainflix.png";
+import higherLower from "../../assests/images/higher-lower.png";
+import inStock from "../../assests/images/inStock.png";
+import bandsite from "../../assests/images/bandsite.png";
+import travelSite from "../../assests/images/travel-site.png";
 
 function Projects() {
   return (
@@ -10,21 +18,79 @@ function Projects() {
           <h2 className="projects__title">Projects</h2>
 
           <div className="proj">
-            <div className="proj__container">
-              <Project
-                screenshot={journal_screenshot}
-                title="Journal"
-                description="An app to showcase my film photographs in a well desgined gallery. A
+            <ProjectSideSwap
+              screenshot={journal_screenshot}
+              title="Journal"
+              description="An app to showcase my film photographs in a well desgined gallery. A
               place to bring the photos out of the folder where they were gathering
               digital dust. View the them in different, appealing ways. See the
               locations the photos were taken and add information alongside them"
-                tech1="React"
-                tech2="MySQL"
-                tech3="SASS"
-                github="https://github.com/Cronkmeister/journal"
-                website=""
-              />
-            </div>
+              tech1="React"
+              tech2="MySQL"
+              tech3="SASS"
+              github="https://github.com/Cronkmeister/journal"
+              website=""
+            />
+            <Project
+              screenshot={dad_jokes}
+              title="Dad Joke Generator"
+              description="Playing around with the OpenAI api. Enter a topic and let the generator tell you a jokes. Nevermind the Turing Test, this is the true test for AI"
+              tech1="Next.js"
+              tech2="OpenAI api"
+              tech3="CSS"
+              github="https://github.com/Cronkmeister/joke-generator"
+              website="https://joke-generator-three.vercel.app/"
+            />
+            <ProjectSideSwap
+              screenshot={brainflix}
+              title="Brainflix"
+              description="A webpage to simulate a video streaming platform"
+              tech1="React"
+              tech2="JSON"
+              tech3="Express"
+              github=""
+              website=""
+            />
+            <Project
+              screenshot={higherLower}
+              title="Higher or Lower"
+              description="Replicating the classic card game: 'Higher or Lower.' See how many in a row you can get!"
+              tech1="Javascript"
+              tech2="HTML"
+              tech3="SASS"
+              github="https://github.com/Cronkmeister/Higher-Or-Lower"
+              website=""
+            />
+            <ProjectSideSwap
+              screenshot={inStock}
+              title="In Stock"
+              description="Replicating an inventory website with a backend server."
+              tech1="React"
+              tech2="Express"
+              tech3="SASS"
+              github="https://github.com/Cronkmeister/inStock"
+              website=""
+            />
+            <Project
+              screenshot={bandsite}
+              title="Band Site"
+              description="The homepage for a fictional band"
+              tech1="HTML"
+              tech2="SASS"
+              tech3="Javascript"
+              github=""
+              website=""
+            />
+            <ProjectSideSwap
+              screenshot={travelSite}
+              title="Travel Site"
+              description=""
+              tech1="HTML"
+              tech2=""
+              tech3="SASS"
+              github=""
+              website=""
+            />
           </div>
         </div>
       </section>
